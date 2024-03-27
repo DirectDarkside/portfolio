261d7b58f7050b01e5f7c8204c30d7197445a298
DirectDarkside <florianmkube13@gmail.com>
2024-03-27 14:04:50 +0100

    change header and above page design

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from '../../assets/shared/components/header/header.component';
 import { LandingComponent } from './landing/landing.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [HeaderComponent, LandingComponent],
+  imports: [LandingComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
