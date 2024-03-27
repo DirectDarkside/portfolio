7c531705fd5563970378ed81582e83f0bb6f186c
DirectDarkside <florianmkube13@gmail.com>
2024-03-27 14:47:18 +0100

    fix header and main-content

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { LandingComponent } from './landing/landing.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { HeaderComponent } from '../../assets/shared/components/header/header.component';
 
 @Component({
   selector: 'app-main-content',
+  host: {
+    class:'main-content'
+  },
   standalone: true,
-  imports: [LandingComponent],
+  imports: [LandingComponent, AboutMeComponent, HeaderComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
