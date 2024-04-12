18c7e4607a3f5e0c3e2e9b9765966185baca7a49
DirectDarkside <florianmkube13@gmail.com>
2024-04-12 11:53:16 +0200

    added translate { header comp }

--- src/app/main-content/landing/landing.component.ts
+++ src/app/main-content/landing/landing.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TranslationService } from '../../services/translation.service';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-landing',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './landing.component.html',
   styleUrl: './landing.component.scss'
 })
 export class LandingComponent {
-  mail = 'kube.florian13@gmail.com'
+  mail = 'kube.florian13@gmail.com';
+  translate = inject(TranslationService);
 }
--- src/assets/shared/components/header/header.component.ts
+++ src/assets/shared/components/header/header.component.ts
@@ -1,17 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../../../../app/services/translation.service';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, TranslateModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
 
   popup: boolean = false;
+  translate = inject(TranslationService);
 
   changeStatus() {
     !this.popup ? this.popup = true : this.popup = false;
