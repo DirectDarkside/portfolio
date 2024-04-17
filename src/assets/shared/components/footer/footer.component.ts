e4d527db954d315fe18f50a3fe43b245db137a08
DirectDarkside <109902468+DirectDarkside@users.noreply.github.com>
2024-04-17 10:32:17 +0200

    finish translate en and de

--- src/assets/shared/components/footer/footer.component.ts
+++ src/assets/shared/components/footer/footer.component.ts
@@ -1,15 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { TranslationService } from '../../../../app/services/translation.service';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, TranslateModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
   mail = 'kube.florian13@gmail.com';
+  translate = inject(TranslationService);
 
   goTopPage() {
     window.scroll(0, 0);
